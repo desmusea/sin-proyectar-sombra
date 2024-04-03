ffaf088fb52eb8117ceb0497ba75e2597ae1b1cc
Glitch (glitch-hello-website) <none>
2024-04-03 16:03:17 +0000

    💃🍃 Checkpoint ./style.css:22712774/5088 ./index.html:22712774/30363 ./script.js:22712774/2849

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  if (n >= slides.length) { slideIndex = 0; }
+  if (n < 0) { slideIndex = slides.length - 1; }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
